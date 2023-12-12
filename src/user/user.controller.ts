3477c5bacd670adeab3757299d3b94e68b915949
Marwam Malta <marwamprog.js@gmail.com>
2023-12-11 23:48:21 -0300

    CRUD criado e testado

---
+++ src/user/user.controller.ts
@@ -0,0 +1,49 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put } from "@nestjs/common";
+import { CreateUserDTO } from "./dto/create-user.dto";
+import { UpdatePutUserDTO } from "./dto/update-put-user.dto";
+import { UpdatePatchUserDTO } from "./dto/update-patch-user.dto";
+
+@Controller('users')
+export class UserController {
+
+    @Post()
+    async create(@Body() {email, name, password}: CreateUserDTO) {
+        return {email, name, password};
+    }
+
+    @Get()
+    async read() {
+        return {users:[]};
+    }
+
+    @Get(':id')
+    async readOne(@Param('id', ParseIntPipe) id) {
+        return {user:{}, id};
+    }
+
+    @Put(':id')
+    async update(@Body() {email, name, password}: UpdatePutUserDTO, @Param('id', ParseIntPipe) id) {
+        return {
+            method: 'put',
+            email, name, password,
+            id
+        };
+    }
+
+    @Patch(':id')
+    async updatePartial(@Body() {email, name, password}: UpdatePatchUserDTO, @Param('id', ParseIntPipe) id) {
+        return {
+            method: 'patch',
+            email, name, password,
+            id
+        };
+    }
+
+    @Delete(':id')
+    async delete(@Param('id', ParseIntPipe) id) {
+        return {
+            id
+        }
+    }
+
+}
