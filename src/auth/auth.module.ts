804464e712382d13e4d9dd3321aeeaad2a873dcf
Marwam Malta <marwamprog.js@gmail.com>
2023-12-14 09:54:49 -0300

    feat: Gerando token e autenticando

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,20 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { AuthController } from "./auth.controller";
+import { UserModule } from "src/user/user.module";
+import { PrismaModule } from "src/prisma/prisma.module";
+import { AuthService } from "./auth.service";
 
 @Module({
-    imports: [JwtModule.register({
-        secret: `cJwvg4Bcjlw0=EF|w9+#Z@g8=KlF<bX*`
-    })]
+    imports: [
+        JwtModule.register({
+            secret: `cJwvg4Bcjlw0=EF|w9+#Z@g8=KlF<bX*`
+        }),
+        UserModule,
+        PrismaModule,
+    ],
+    controllers: [AuthController],
+    providers: [AuthService]
 })
 export class AuthModule {
 
